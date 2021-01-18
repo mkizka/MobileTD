efc92e5bb6eaf24c7e2480be8eee435daf9109b1
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-18 19:34:36 +0900

    単一の文字列変数としてexportされるように

--- observer/rollup.config.js
+++ observer/rollup.config.js
@@ -1,10 +1,22 @@
 import typescript from "@rollup/plugin-typescript";
+import replace from "rollup-plugin-re";
 
 export default {
   input: "./src/index.ts",
   output: {
-    file: "./dist/index.txt",
-    format: "iife",
+    file: "./dist/index.js",
+    format: "esm",
   },
-  plugins: [typescript()],
+  plugins: [
+    typescript(),
+    replace({
+      patterns: [
+        {
+          transform(code) {
+            return `export default \`\`\`${code}\`\`\`;`;
+          },
+        },
+      ],
+    }),
+  ],
 };
