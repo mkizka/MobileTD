decb98b929ec398116df562bdc837478a7862e7e
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-19 01:19:12 +0900

    更新

--- observer/src/index.ts
+++ observer/src/index.ts
@@ -1,4 +1,21 @@
-alert("bar");
-(window as any).observer = new MutationObserver(() => {
-  console.log("foo");
+import { createTweetDeckState } from "./state";
+
+const columnObserver = new MutationObserver((records) => {
+  // ツイートリストを通知
+  records.forEach((record) => {
+    const columns = (record.target as HTMLElement).querySelectorAll<HTMLElement>(
+      ".js-column"
+    );
+    const message = JSON.stringify(createTweetDeckState([...columns]));
+    window.ReactNativeWebView.postMessage(message);
+  });
+});
+
+const containerObserver = new MutationObserver((records) => {
+  records.forEach((record) => {
+    // カラム追加時に新規に監視を開始
+    record.addedNodes.forEach((column) => {
+      columnObserver.observe(column, { childList: true });
+    });
+  });
 });
---
+++ observer/src/state.ts
@@ -0,0 +1,59 @@
+export type TweetDeckState = {
+  columns: ColumnSection[];
+};
+
+export function createTweetDeckState(sections: HTMLElement[]): TweetDeckState {
+  return { columns: sections.map((section) => createColumnSection(section)) };
+}
+
+export type ColumnSection = {
+  columnId: string;
+  tweets: TweetArticle[];
+};
+
+function createColumnSection(section: HTMLElement) {
+  const tweetItems = section.querySelectorAll<HTMLElement>(".js-stream-item");
+  return {
+    columnId: section.dataset.column!,
+    tweets: [...tweetItems].map((article) => createTweetArticle(article)),
+  };
+}
+
+export type TweetArticle = {
+  user: TweetUser;
+  text: string;
+  repliesCount: number;
+  retweetsCount: number;
+  favoritesCount: number;
+};
+
+function createTweetArticle(artice: HTMLElement): TweetArticle {
+  return {
+    user: createTweetUser(
+      artice.querySelector<HTMLElement>(".js-tweet-heaer")!
+    ),
+    text: text(artice, ".js-tweet-text"),
+    repliesCount: count(artice, ".js-reply-count"),
+    retweetsCount: count(artice, ".js-retweet-count"),
+    favoritesCount: count(artice, ".js-like-count"),
+  };
+}
+export type TweetUser = {
+  name: string;
+  screenName: string;
+};
+
+function createTweetUser(header: HTMLElement): TweetUser {
+  return {
+    name: text(header, ".fullname"),
+    screenName: text(header, ".username"),
+  };
+}
+
+function text(el: HTMLElement, query: string, _default: string = ""): string {
+  return el.querySelector(query)!.textContent || _default;
+}
+
+function count(el: HTMLElement, query: string, _default: number = 0): number {
+  return parseInt(text(el, query)) || _default;
+}
