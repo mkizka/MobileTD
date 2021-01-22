6258a30e0c78f44853ec11ed7cf4cb28d905fa86
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-22 20:11:29 +0900

    react-native-render-html追加

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { View } from "react-native";
 import { Card, Text } from "react-native-elements";
+import HTML from "react-native-render-html";
+
 import { TweetArticle } from "../../observer";
 
 export const Tweet: React.FC<{ tweet: TweetArticle }> = ({ tweet }) => {
@@ -7,7 +10,11 @@ export const Tweet: React.FC<{ tweet: TweetArticle }> = ({ tweet }) => {
     <Card>
       <Text h4>{tweet.user.name}</Text>
       <Text>{tweet.user.screenName}</Text>
-      <Text>{tweet.text}</Text>
+      <HTML
+        containerStyle={{ flexDirection: "row", flexWrap: "wrap" }}
+        classesStyles={{ emoji: { width: 16, height: 16 } }}
+        source={{ html: tweet.text }}
+      />
     </Card>
   );
 };
