dba4ef7cd4dad670025e003d1234bfc2eefd6cc5
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-23 01:04:30 +0900

    wip

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View } from "react-native";
-import { Card, Text } from "react-native-elements";
+import { FlatList, View, StyleSheet } from "react-native";
+import { Card, Text, Image } from "react-native-elements";
 import HTML from "react-native-render-html";
+import Lightbox from "react-native-lightbox";
 
 import { TweetArticle } from "../../observer";
 
@@ -10,11 +11,36 @@ export const Tweet: React.FC<{ tweet: TweetArticle }> = ({ tweet }) => {
     <Card>
       <Text h4>{tweet.user.name}</Text>
       <Text>{tweet.user.screenName}</Text>
-      <HTML
-        containerStyle={{ flexDirection: "row", flexWrap: "wrap" }}
-        classesStyles={{ emoji: { width: 16, height: 16 } }}
-        source={{ html: tweet.text }}
-      />
+      {tweet.text ? (
+        <HTML
+          containerStyle={{ flexDirection: "row", flexWrap: "wrap" }}
+          classesStyles={{ emoji: { width: 16, height: 16 } }}
+          source={{ html: tweet.text }}
+        />
+      ) : null}
+      {tweet.thumbnailUrls.length > 0 ? (
+        <FlatList
+          style={styles.mediaContainer}
+          data={tweet.thumbnailUrls}
+          numColumns={2}
+          columnWrapperStyle={{ width: "50%", height: "50%" }}
+          renderItem={({ item, index }) => (
+            <Lightbox key={index}>
+              <Image
+                source={{ uri: item }}
+                resizeMode="cover"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </Lightbox>
+          )}
+        />
+      ) : null}
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  mediaContainer: {
+    height: 130,
+  },
+});
---
+++ src/types.d.ts
@@ -0,0 +1,24 @@
+// From https://github.com/cbbfcd/react-native-lightbox/blob/master/types.d.ts
+declare module 'react-native-lightbox' {
+  import { Animated, StyleProp, ViewStyle } from 'react-native'
+
+  interface LightboxProps {
+    activeProps?: any
+    renderHeader?: (close: () => void) => void
+    renderContent?: () => void
+    underlayColor?: string
+    backgroundColor?: string
+    didOpen?: () => void
+    onOpen?: () => void
+    willClose?: () => void
+    onClose?: () => void
+    springConfig?: Animated.SpringAnimationConfig['friction' | 'tension']
+    swipeToDismiss?: boolean
+    style?: StyleProp<ViewStyle>
+    onLongPress?: (...args: any) => void
+  }
+
+  const Lightbox: React.FC<LightboxProps>
+
+  export default Lightbox
+}
