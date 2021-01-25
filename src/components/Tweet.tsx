56a2c61a665a20b033d59754740d3b20eb334680
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-25 17:42:26 +0900

    ユーザアイコン表示と調整

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TouchableHighlight, Modal } from "react-native";
-import { Card, Text, Image } from "react-native-elements";
+import {
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  Modal,
+  ActivityIndicator,
+} from "react-native";
+import { Avatar, Text, Image, ListItem, Card } from "react-native-elements";
 import HTML from "react-native-render-html";
 import ImageViewer from "react-native-image-zoom-viewer";
 
@@ -19,7 +25,7 @@ export const Tweet: React.FC<{ tweet: TweetArticle }> = ({ tweet }) => {
       <TouchableHighlight onPress={handlePress} style={{ flex: 1 }}>
         <Image
           source={{ uri: tweet.thumbnailUrls[index] }}
-          containerStyle={styles.thumbnail}
+          containerStyle={styles.thumbnailItem}
         />
       </TouchableHighlight>
     );
@@ -27,53 +33,90 @@ export const Tweet: React.FC<{ tweet: TweetArticle }> = ({ tweet }) => {
 
   return (
     <Card>
-      <Text h4>{tweet.user.name}</Text>
-      <Text>{tweet.user.screenName}</Text>
-      {tweet.text ? (
-        <HTML
-          containerStyle={{ flexDirection: "row", flexWrap: "wrap" }}
-          classesStyles={{ emoji: styles.emoji, hashflag: styles.emoji }}
-          source={{ html: tweet.text }}
-        />
-      ) : null}
-      {tweet.thumbnailUrls.length >= 1 && (
-        <View style={styles.thumbnailsContainer}>
-          <View style={styles.thumbnailsColumn}>
-            <ThumbnailImage index={0} />
-            {tweet.thumbnailUrls.length == 4 && (
-              // 4画像時の3枚目は1列目に移動するため
-              <ThumbnailImage index={2} />
-            )}
+      <View style={styles.inner}>
+        <View style={styles.leftColumn}>
+          {tweet.user.profileImageUrl ? (
+            <Avatar
+              rounded
+              //containerStyle={styles.avatar}
+              overlayContainerStyle={styles.avatar}
+              renderPlaceholderContent={<ActivityIndicator />}
+              title={tweet.user.name}
+              source={{ uri: tweet.user.profileImageUrl }}
+            />
+          ) : null}
+        </View>
+        <View style={styles.rightColumn}>
+          <View style={styles.rightColumnHeader}>
+            <Text style={styles.userName}>{tweet.user.name}</Text>
+            <Text style={styles.screenName}>{tweet.user.screenName}</Text>
           </View>
-          {tweet.thumbnailUrls.length >= 2 && (
-            <View style={styles.thumbnailsColumn}>
-              {/* 2枚、3枚、4枚のときの画像インデックスの組み合わせ */}
-              {[[1], [1, 2], [1, 3]][tweet.thumbnailUrls.length - 2].map(
-                (index) => (
-                  <ThumbnailImage key={index} index={index} />
-                )
+          {tweet.text ? (
+            <HTML
+              containerStyle={{ flexDirection: "row", flexWrap: "wrap" }}
+              classesStyles={{ emoji: styles.emoji, hashflag: styles.emoji }}
+              source={{ html: tweet.text }}
+            />
+          ) : null}
+          {tweet.thumbnailUrls.length >= 1 && (
+            <View style={styles.thumbnailsContainer}>
+              <View style={styles.thumbnailsColumn}>
+                <ThumbnailImage index={0} />
+                {tweet.thumbnailUrls.length == 4 && (
+                  // 4画像時の3枚目は1列目に移動するため
+                  <ThumbnailImage index={2} />
+                )}
+              </View>
+              {tweet.thumbnailUrls.length >= 2 && (
+                <View style={styles.thumbnailsColumn}>
+                  {/* 2枚、3枚、4枚のときの画像インデックスの組み合わせ */}
+                  {[[1], [1, 2], [1, 3]][tweet.thumbnailUrls.length - 2].map(
+                    (index) => (
+                      <ThumbnailImage key={index} index={index} />
+                    )
+                  )}
+                </View>
               )}
             </View>
           )}
+          {modalOpen && (
+            <Modal visible={true} transparent={true}>
+              <ImageViewer
+                index={imageIndex}
+                imageUrls={tweet.thumbnailUrls.map((url) => {
+                  return { url };
+                })}
+                enableSwipeDown={true}
+                onSwipeDown={() => setModalOpen(false)}
+              />
+            </Modal>
+          )}
         </View>
-      )}
-      {modalOpen && (
-        <Modal visible={true} transparent={true}>
-          <ImageViewer
-            index={imageIndex}
-            imageUrls={tweet.thumbnailUrls.map((url) => {
-              return { url };
-            })}
-            enableSwipeDown={true}
-            onSwipeDown={() => setModalOpen(false)}
-          />
-        </Modal>
-      )}
+      </View>
     </Card>
   );
 };
 
 const styles = StyleSheet.create({
+  inner: {
+    flexDirection: "row",
+  },
+  leftColumn: {
+    width: 50,
+  },
+  rightColumn: {
+    flex: 1,
+  },
+  rightColumnHeader: {
+    flexDirection: "row"
+  },
+  userName: {
+    fontWeight: "bold",
+  },
+  screenName: {
+    color: "grey",
+    fontSize: 10,
+  },
   thumbnailsContainer: {
     height: 130,
     width: "100%",
@@ -82,7 +125,7 @@ const styles = StyleSheet.create({
   thumbnailsColumn: {
     flex: 1,
   },
-  thumbnail: {
+  thumbnailItem: {
     flex: 1,
     resizeMode: "cover",
   },
